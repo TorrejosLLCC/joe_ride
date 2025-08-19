493b3b35b78d02a87f510db902b73a45151fcfdc
Mamonde02 <valerosojohnraymund@gmail.com>
2025-08-19 17:40:20 +0800

    fix api endpoint and types data

--- frontend/src/api/rides/offerRideApi.ts
+++ frontend/src/api/rides/offerRideApi.ts
@@ -1,21 +1,33 @@
 import api from "../index";
 
 export interface OfferRidePayload {
-    origin: string;
-    destination: string;
-    departureDate: string;
+    fromLocation: string;
+    toLocation: string;
+    departure: string;
     departureTime: string;
     capacity: number;
     vehicleType: string;
-    kilometerCount: number;
+    distanceKm: number;
 }
 
 export const createOfferRide = async (data: OfferRidePayload) => {
-    const res = await api.post("/rides/offer", data);
+    const userId = localStorage.getItem("userId");
+
+    console.log(userId);
+
+    if (!userId) {
+        throw new Error("No userId found in localStorage. Please login first.");
+    }
+
+    const res = await api.post("/ride-offers", data, {
+        headers: {
+            "x-user-id": userId
+        },
+    });
     return res.data;
 };
 
 export const getAllRideOffers = async () => {
-    const res = await api.get("/rides/offers");
+    const res = await api.get("/ride-offers");
     return res.data;
 };
